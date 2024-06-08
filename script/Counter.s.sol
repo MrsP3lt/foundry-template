1523711bca5a6413e1e4b82864b317511c9b6111
EmileSean <emilesean.es@gmail.com>
2024-06-08 10:43:51 +0300

    Update contracts to use soldeer
    Depreciate .gitmodules

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import "forge-std/Script.sol";
-
+import "@forge-std-1.8.2/Script.sol";
 contract CounterScript is Script {
 
     function setUp() public {}
